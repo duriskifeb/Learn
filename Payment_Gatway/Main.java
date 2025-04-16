1fb40b5ba4fa40767ffc2950265ac8729a5d6184
duriskifeb <rizkydurfeb976@gmail.com>
2025-04-16 19:02:09 +0700

    clear ya

--- Payment_Gatway/Main.java
+++ Payment_Gatway/Main.java
@@ -1,5 +1,36 @@
 package Payment_Gatway;
 
+import java.util.Scanner;
+
+import Payment_Gatway.Model.BankTransferPayment;
+import Payment_Gatway.Model.CreditCardPayment;
+import Payment_Gatway.Model.EWalletPayment;
+import Payment_Gatway.Model.Payment;
+import Payment_Gatway.Model.Transaction;
+import Payment_Gatway.Service.PaymentProcces;
+import Payment_Gatway.Service.TransactionHistory;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TransactionHistory history = new TransactionHistory();
+        PaymentProcces processor = new PaymentProcces(history);
+        Payment creditCard = new CreditCardPayment();
+        Payment eWallet = new EWalletPayment();
+        Payment bankTransfer = new BankTransferPayment();
+        //simulasi transaksi
+        processor.processTrancaction("TXN001", 100, creditCard);
+        processor.processTrancaction("TXN002", 200, eWallet);
+        processor.processTrancaction("TXN003", 300, bankTransfer);
+        // menampilkan riwayat transaksi setelah pembayaran di lakukan
+        history.showTransaction();
+        // menyampaikan fitur interaktif untuk melihat riwayat transaksi
+        System.out.println("\n Ingin melihat riwayat transaksi lagi? (y/n)");
+        String input = scanner.nextLine();
+        if (input.equalsIgnoreCase("y")) {
+            history.showTransaction();
+        }
+        System.out.println("Terimakasih telah menggunakan sistem pembayaran!");
+        scanner.close();
+    }
 }
--- Payment_Gatway/Service/TransactionHistory.java
+++ Payment_Gatway/Service/TransactionHistory.java
@@ -1,7 +1,24 @@
 package Payment_Gatway.Service;
 
-public class TransactionHistory {
-    
-}
+import java.util.ArrayList;
+import java.util.List;
 
-////
+import Payment_Gatway.Model.Transaction;
+
+public class TransactionHistory {
+    private List<Transaction> transactionss = new ArrayList<>();
+
+    public void addTransactionHistory(Transaction transaction) {
+        transactionss.add(transaction);
+    }
+
+    public void showTransaction() {
+        System.err.println("\n - Riwayat Transaksi -");
+        if (transactionss.isEmpty()) {
+            System.out.println("Belum ada transaksi.");
+        }else{
+            for(Transaction transaction : transactionss)
+            System.out.println(transaction);
+        }
+    }
+}
